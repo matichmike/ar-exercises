0a6c205a2f35b75a77bf6c5e1ab5162bb7ddf15a
Michael Matich <matich.mike@gmail.com>
2020-05-26 03:57:19 +0000

    ex 1-7 completed

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
+  belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
-
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: 0}
 end
